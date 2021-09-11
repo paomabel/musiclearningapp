1d8399de5979722fc69b3c6aea4aafc0ee2ffc13
paomabel <70816537+paomabel@users.noreply.github.com>
2021-09-11 16:49:37 -0500

    Add files via upload
    new

--- main.py
+++ main.py
@@ -2,33 +2,48 @@
 import pygame
 pygame.init()
 
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((1250,700))
 pygame.display.set_caption("First Game")
 x = 50
-y = 50
-width = 2000
-height = 1000
+y = 550
+width = 200
+height = 100
 vel = 5 
 run = True
 left = False
 right = False
 isjump = False
-jcount = 0
+jcount = 10
 walknum = 0
 
-walkright = [pygame.image.load('gamepictures/csright.png'),pygame.image.load('gamepictures/cs2right.png')]
-walkleft = [pygame.image.load('gamepictures/characterspriteleft.png'),pygame.image.load('gamepictures/charactersprite2left.png')]
+clock = pygame.time.Clock()
+
+walkright = [pygame.image.load('gamepictures/csright.png')]#,pygame.image.load('gamepictures/cs2right.png')]
+walkleft = [pygame.image.load('gamepictures/csleft.png')]#,pygame.image.load('gamepictures/charactersprite2left.png')]
 bg = pygame.image.load('gamepictures/bg.png')
-charact = [pygame.image.load('gamepictures/csright.png'),pygame.image.load('gamepictures/cs2right.png')]
+charact = pygame.image.load('gamepictures/csright.png') #,pygame.image.load('gamepictures/cs2right.png')]
 
 def redrawGamewindow():
     global walknum
     win.blit(bg, (0,0))
-    pygame.draw.rect(win,(255,0,0),(x,y,width,height))
+    if walknum+1 >= 32:
+        walknum = 0
+    if right:
+        win.blit(walkright[walknum//3],(x,y))
+        walknum += 1
+    elif left:
+        win.blit(walkleft[walknum//3],(x,y))
+        walknum+=1
+    else:
+        win.blit(charact,(x,y))
+
+    #pygame.draw.rect(win,(255,0,0),(x,y,width,height))
     pygame.display.update()
 
 while run:
-    pygame.time.delay(100)
+
+    pygame.time.delay(50)
+    clock.tick(20)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -43,20 +58,24 @@ while run:
         left = False
     else:
         right = False
-        left = True
+        left = False
         walknum = 0
     if not(isjump):
         if keys[pygame.K_SPACE]:
             isjump = True
+            right = False
+            left = False
+            walknum = 0
     else:
         if jcount >= -10:
             neg = 1
             if jcount < 0:
                 neg = -1
-            y -= (jcount **2) *.5 *neg
+            y -= (jcount **2) *0.5 *neg
             jcount -= 1
         else:
-            isjump = Falsejcount = 10
+            isjump = False
+            jcount = 10
     redrawGamewindow()
 pygame.quit()
 #playsound('import.mp3')
